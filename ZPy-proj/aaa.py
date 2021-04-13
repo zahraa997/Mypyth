fd28fed17a67ed0989a2333f002ede48f5b3037c
zahraa997 <80203419+zahraa997@users.noreply.github.com>
2021-04-13 23:02:19 +0400

    Add files via upload

---
+++ ZPy-proj/aaa.py
@@ -0,0 +1,10 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("program to print the modulus of the numbers")
+program to print the modulus of the numbers
+>>> num1=25
+>>> num2=3
+>>> result=num1%num2
+>>> print(result)
+1
+>>> 
